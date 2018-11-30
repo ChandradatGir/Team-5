7856c0d1fc31de519fd7d48d0e6a29d10221f6ba
Chandradat Gir <chandradatgir@yahoo.com>
2018-11-30 18:45:22 -0500

    Initial project files

---
+++ js/item-list.js
@@ -0,0 +1,47 @@
+ var itemData = {
+	ha1:{
+		name: 'Whirpool Refrigerator',
+		price: 1498.00,
+    quantity:0.00,
+    cost:0.00,
+		type: 'Home Appliances',
+		describe: 'Whirpool 25 cu. ft French Door Refrigetor',
+    image:'images/whirlpool-french-door-refrigerators.jpg',
+
+	},
+	ha2:{
+		name: 'Samsung Gas Range',
+    price: 598.00,
+    quantity:0.00,
+    cost:0.00,
+    type: 'Home Appliances',
+    describe: 'Samsung Self-Cleaning Sainless Steel Gas Range',
+    image:'images/samsung-gas-ranges.jpg',
+	},
+	ha3:{
+	name: 'Instant Pot',
+	price: 70.00,
+	quantity:0.00,
+	type: 'Home Appliances',
+	describe: 'Instant Pot Duo 6qt 7-in-1 Pressure Cooker',
+	image:'images/instant-pot.jpg',
+},
+ha4:{
+	name: 'LG Countertop Microwave',
+	price: 132.00,
+	quantity:0.00,
+	cost:0.00,
+	type: 'Home Appliances',
+	describe: 'LG 0.9 cu ft Countertop Microwave',
+	image: 'images/countertop-microwave.jpg',
+},
+ha5:{
+	name: 'Ninja Pro Kitchen System',
+	price: 160.00,
+	quantity:0.00,
+	cost:0.00,
+	type: 'Home Appliances',
+	describe: 'Ninja Professional 1200W Kitchen System',
+	image: 'images/ninja-blender.jpg',
+}
+};
