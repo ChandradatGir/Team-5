5ed4da9cd45d7e4b034348a8f9549766ef2f12eb
Chandradat Gir <chandradatgir@yahoo.com>
2018-12-04 19:48:47 -0500

    creating the shopping cart list

---
+++ js/keywords-list.js
@@ -0,0 +1,5 @@
+var wordsData=["great", "cheap", "fantastic", "reliable",
+  "love product", "works better", "not as expected", "expected",
+ "Very unhappy", "happy", "Excellent", "love","better",
+ "works","defective", "broken", "cracked", "missing"
+];
